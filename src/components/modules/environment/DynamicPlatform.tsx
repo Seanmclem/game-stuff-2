4d05507d3d301c8b59b3408014e6740fd8024ec0
Seanmclem <seanmclements@gmail.com>
2024-02-17 15:13:11 -0500

    vars

--- src/components/modules/environment/DynamicPlatform.tsx
+++ src/components/modules/environment/DynamicPlatform.tsx
@@ -12,9 +12,14 @@ const block_size = 2;
 export const DynamicPlatform = ({
   position = [0, 0, 0],
   forwardDepth = 0,
-  sideWidth = 0,
+  sideWidth = 2,
 }: SimplePlatformProps) => {
   const degrree_to_radian = (degree: number) => degree * (Math.PI / 180);
+
+  const topRight_moveRight = -(sideWidth * block_size) / 2;
+  const topLeft_moveLeft = (sideWidth * block_size) / 2;
+  const bottomRight_moveRight = -(sideWidth * block_size) / 2;
+  const bottomLeft_moveLeft = (sideWidth * block_size) / 2;
 
   return (
     <group position={position as Vector3}>
@@ -23,7 +28,7 @@ export const DynamicPlatform = ({
         physicsProps={{
           type: "fixed",
           position: [
-            position[0] - (sideWidth * block_size) / 2,
+            position[0] + topRight_moveRight,
             position[1],
             position[2],
           ],
@@ -37,7 +42,7 @@ export const DynamicPlatform = ({
         physicsProps={{
           type: "fixed",
           position: [
-            position[0] + block_size + (sideWidth * block_size) / 2,
+            position[0] + block_size + topLeft_moveLeft,
             position[1],
             position[2],
           ],
@@ -54,7 +59,7 @@ export const DynamicPlatform = ({
         physicsProps={{
           type: "fixed",
           position: [
-            position[0] + block_size + (sideWidth * block_size) / 2,
+            position[0] + block_size + bottomLeft_moveLeft,
             position[1],
             position[2] - block_size, //- forwardDepth * block_size,
           ],
@@ -69,7 +74,7 @@ export const DynamicPlatform = ({
         physicsProps={{
           type: "fixed",
           position: [
-            position[0] - (sideWidth * block_size) / 2,
+            position[0] + bottomRight_moveRight,
             position[1],
             position[2] - block_size,
           ],
